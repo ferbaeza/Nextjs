401ef071c780d15702bfb538dfeb8a632042c86a
ferbaeza <fbaezahurtado@gmail.com>
2021-11-19 16:33:32 +0100

    next_deve

---
+++ ferbaeza/pages/view1.js
@@ -0,0 +1,11 @@
+
+
+const Hola = () =>(
+    <section className="hola">
+    <a href="/">Back</a>
+    <h2 className="text-center text-white">Hola</h2>
+    <p>Editing by FerBaeza</p>
+
+    </section>
+);
+export default Hola;
