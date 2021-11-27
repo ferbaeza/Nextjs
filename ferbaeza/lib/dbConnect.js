93bacb01f7d5a673f20a343d43c14d119c3ee3e3
ferbaeza <fbaezahurtado@gmail.com>
2021-11-27 15:14:42 +0100

    atlas

--- ferbaeza/lib/dbConnect.js
+++ ferbaeza/lib/dbConnect.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose'
 const MONGODB_URI = process.env.MONGODB_URI
 
 if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
+  //throw new Error(
+    console.log('Please define the MONGODB_URI environment variable inside .env.local')
+  
 }
 const dbConnect = async()=>{
     try {
@@ -26,35 +26,3 @@ const dbConnect = async()=>{
     }
 }
 export default dbConnect;
-
-
-/*
-let cached = global.mongoose
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      bufferCommands: false,
-      bufferMaxEntries: 0,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose
-    })
-  }
-  cached.conn = await cached.promise
-  return cached.conn
-}
-*/
--- ferbaeza/pages/atlas.js
+++ ferbaeza/pages/atlas.js
@@ -2,45 +2,10 @@ import dbConnect from '../lib/dbConnect'
 import firstApp from '../models/firstApp'
 
 
-
-
-function Home({ users }) {
-    console.log(users);
-    return (
-      <div>
-  
-        <main className="container">
-          <h1 className="my-3 text-center">Users</h1>
-  
-  
-          {users.map(({ _id, name, surname, mail }) => (
-            <div className="card mb-2" key={_id}>
-              <div className="card-body">
-                <p className="text-uppercase h5">{name}</p>
-                <p className="fw-light">{surname}</p>
-                <p className="fw-light">{mail}</p>
-
-                <div>
-                  <Link href="/[id]/edit" as={`/${_id}/edit`}>
-                    <a className="btn btn-warning me-2">Editar</a>
-                  </Link>
-                  <Link href="/[id]" as={`/${_id}`}>
-                    <a className="btn btn-info">Visualizar</a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          ))}
-        </main>
-      </div>
-    );
-  }
-
-export async function getServerSideProps(){
+export default async function getServerSideProps(){
     try {
         await dbConnect();
-        
-            const res = await firstApp.find({});
+            //const res = await firstApp.find({});
 
             const users = result.map((doc)=>{
             const user = doc.toObject();
@@ -48,11 +13,9 @@ export async function getServerSideProps(){
             return user;
         })
 
-        console.log(res);
+        console.log(user);
 
-        return {props: {name: fer }};
     }   catch (error) {
         console.log(error);
     }
 }
-export default Home()
